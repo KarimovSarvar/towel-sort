7bb8728783bbc48b5a8b4752def3e737a71d5238
Sarvar <ksz95@bk.ru>
2023-08-23 13:07:27 +0600

    feat: make towel sort

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    if (matrix == undefined) return [];
+    if(matrix == undefined) return [];
+    else {
+        const newMatrix =  matrix.map(function(item,index) {
+            if(index%2 == 0) {
+                return item
+            }
+            else return item.reverse()
+        } )
+        return newMatrix.flat()
+    }
+    /*if (matrix == undefined) return [];
     else {
     let newMatrix = [];
     for (let i = 0; i < matrix.length; i++) {
@@ -14,5 +24,5 @@ module.exports = function towelSort (matrix) {
         }
     }
     return newMatrix
- }
+ }*/
 }
